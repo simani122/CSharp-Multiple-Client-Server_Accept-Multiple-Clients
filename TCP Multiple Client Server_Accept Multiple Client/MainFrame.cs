a9dcf550c4a5b38131b8528442235c7cc8e490c6
simani122 <45187937+simani122@users.noreply.github.com>
2021-06-15 16:41:54 +0900

    asd

---
+++ TCP Multiple Client Server_Accept Multiple Client/MainFrame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_Multiple_Client_Server_Accept_Multiple_Client
+{
+    public partial class MainFrame : Form
+    {
+        public MainFrame()
+        {
+            InitializeComponent();
+            //
+        }
+    }
+}
